dfde2cbf28aedda25b1b6137b475685e68cda718
daithioc95 <51501850+daithioc95@users.noreply.github.com>
2022-11-23 11:10:58 +0000

    Reduce to front end assets only for vercel

--- favourites/serializer.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from . models import *
-from favourites.models import UserFavourites
-  
-class FavouritesSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = UserFavourites
-        fields = ['favourites',
-                'user',]
--- favourites/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-from favourites.views import *
-
-urlpatterns = [
-    path('getuserfavs/', GetUserFavourites.as_view()),
-]
